3ed4863b4ace5d145d7f49e725da5db5337a0f17
nandhu06prabha <153995777+nandhu06prabha@users.noreply.github.com>
2023-12-28 17:42:12 +0530

    Create 1.4B_EmpInfo.java

---
+++ 1.4B_EmpInfo.java
@@ -0,0 +1,10 @@
+public class EmpInfo {
+    public static void main(String[] args) {
+        // Create an Employee object
+        Employee employee = new Employee("Nandhini", 25, 1752040, 41000.0);
+
+        // Display info about the employee
+        System.out.println("Employee Information:");
+        employee.displayEmployeeInfo();
+    }
+}
